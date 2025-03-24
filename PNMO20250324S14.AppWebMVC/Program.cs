f200a3579a88e6aa6968ba4959cfd81dec3ebfa0
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2025-03-24 16:51:02 -0300

    Vance del Ejercicio

--- PNMO20250324S14.AppWebMVC/Program.cs
+++ PNMO20250324S14.AppWebMVC/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PNMO20250324S14.AppWebMVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ClientesDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
