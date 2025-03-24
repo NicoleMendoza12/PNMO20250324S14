4d04ee4cc0a78e4697331efd05eaea0e5165af9d
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2025-03-24 16:31:28 -0300

    Agregar archivos de proyecto.

---
+++ PNMO20250324S14.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PNMO20250324S14.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
