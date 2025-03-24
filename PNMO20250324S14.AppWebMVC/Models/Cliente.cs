07c4351f32c81f46e47fcc75f3ea1baaf390d265
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2025-03-24 17:57:05 -0300

    Nuevos cambios

--- PNMO20250324S14.AppWebMVC/Models/Cliente.cs
+++ PNMO20250324S14.AppWebMVC/Models/Cliente.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PNMO20250324S14.AppWebMVC.Models;
 
 public partial class Cliente
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
-
+    [Display(Name = "Dirección")]
     public string? Direccion { get; set; }
-
+    [Display(Name = "Teléfono")]
     public string? Telefono { get; set; }
-
+    
     public string? Email { get; set; }
 }
